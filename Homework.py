8e784096bd8d0a8d3b8cb5e5f7d26763b81a0c38
Raven3E <shegi007@gmail.com>
2024-11-30 23:06:11 +0700

    30.11

---
+++ Homework.py
@@ -0,0 +1,6 @@
+kol_dz = 12
+hours_spent = 1.5
+course_name = 'Python'
+time_per_task = hours_spent / kol_dz
+print(f'На курсе {course_name} выполнено {kol_dz} заданий за {hours_spent} часов.')
+print(f'Среднее время на одно задание: {time_per_task:.2f} часа.')
