de5e169e581d00b4567b5c910a8ead9ad75f28bb
Raven3E <shegi007@gmail.com>
2025-01-14 03:03:40 +0700

    module_4_1

---
+++ module_4_1.py
@@ -0,0 +1,16 @@
+# module_4_1.py
+
+from fake_math import divide as fake_divide
+from true_math import divide as true_divide
+
+# Пример вызовов функций
+result1 = fake_divide(69, 3)
+result2 = fake_divide(3, 0)
+result3 = true_divide(49, 7)
+result4 = true_divide(15, 0)
+
+# Вывод результатов
+print(result1)  # 23.0
+print(result2)  # Ошибка
+print(result3)  # 7.0
+print(result4)  # inf
